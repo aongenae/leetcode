aa9554f7d70bcb225efd1af0381d1ce63421c11e
Arnaud Ongenae <aongenae@gmail.com>
2018-06-01 14:36:44 -0700

    Problem 15

--- src/test/test_three_sum.py
+++ src/test/test_three_sum.py
@@ -23,27 +23,27 @@ class _Mixin(object):
 
 class TestReverseInteger(LeetcodeTest, _Mixin):
 
-    def not_enabled_yet_test_not_long_enough(self):
+    def test_not_long_enough(self):
         nums = [-1, 0]
         expected = []
         self.validate(nums, expected)
 
-    def not_enabled_yet_test_example(self):
+    def test_example(self):
         nums = [-1, 0, 1, 2, -1, -4]
-        expected = [[-1, 0, 1], [-1, -1, 2]]
+        expected = [(-1, 0, 1), (-1, -1, 2)]
         self.validate(nums, expected)
 
-    def not_enabled_yet_test_no_solution(self):
+    def test_no_solution(self):
         nums = [3, -2, 1, 0]
         expected = []
         self.validate(nums, expected)
 
-    def not_enabled_yet_test_zeros(self):
+    def test_zeros(self):
         nums = [0, 0, 0]
-        expected = [[0, 0, 0]]
+        expected = [(0, 0, 0)]
         self.validate(nums, expected)
 
-    def not_enabled_yet_test_2(self):
+    def test_2(self):
         nums = [-2, 0, 0, 2, 2]
-        expected = [[-2, 0, 2]]
+        expected = [(-2, 0, 2)]
         self.validate(nums, expected)
--- src/three_sum.py
+++ src/three_sum.py
@@ -20,112 +20,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype List[List[int]]
         """
-        print('\n {}'.format(nums))
-        solution = set()
-
-        [
-            solution.add(tuple(sorted([a, b, c])))
-            for idx_a, a in enumerate(nums[:-2], 1)
-            for idx_b, b in enumerate(nums[idx_a:-1], idx_a+1)
-            for c in nums[idx_b:]
-            if a + b + c == 0
-        ]
-        print('-----------------------')
-        print(solution)
-        return [list(k) for k in solution]
-
-    def threeSum_dict(self, nums):
-        """
-        :type nums: List[int]
-        :rtype List[List[int]]
-        """
-        print('\n {}'.format(nums))
-        solution = {}
-
-        def _duplicate_helper(a, b, c):
-            solution[tuple(sorted([a, b, c]))] = None
-
-        [
-            _duplicate_helper(a, b, c)
-            for idx_a, a in enumerate(nums[:-2])
-            for idx_b, b in enumerate(nums[idx_a+1:-1])
-            for c in nums[idx_a+idx_b+2:]
-            if a + b + c == 0
-        ]
-        print('-----------------------')
-        return [list(k) for k in solution]
-
-    def threeSum_comprehension(self, nums):
-        """
-        :type nums: List[int]
-        :rtype List[List[int]]
-        """
-        solution = []
-        length = len(nums)
-        if length < 3:
-            return []
-
-        def _duplicate_helper(a, b, c):
-            candidate = sorted([a, b, c])
-            if candidate not in solution:
-                solution.append(candidate)
-
-        [
-            _duplicate_helper(a, b, c)
-            for idx_a, a in enumerate(nums[:-3])
-            for idx_b, b in enumerate(nums[idx_a+1:-2])
-            for c in nums[idx_b+1:]
-            if a + b + c == 0
-        ]
-        return solution
-
-    def threeSum_loop(self, nums):
-        """
-        :type nums: List[int]
-        :rtype List[List[int]]
-        """
-        print('\n{}'.format(nums))
-        solution = []
-        length = len(nums)
-        if length < 3:
-            return []
-        for idx_a, a in enumerate(nums[:-3]):
-            print('a {}'.format(a))
-            for idx_b, b in enumerate(nums[idx_a+1:-2]):
-                print('b {}'.format(b))
-                for c in nums[idx_b+1:]:
-                    print('c {}'.format(c))
-                    print('{} {} {}'.format(a, b, c))
-                    if a + b + c == 0:
-                        candidate = sorted([a, b, c])
-                        if candidate not in solution:
-                            solution.append(candidate)
-        return solution
-
-    def threeSum_slow(self, nums):
-        """
-        :type nums: List[int]
-        :rtype List[List[int]]
-        """
-        print('\n{}'.format(nums))
-        solution = []
-        length = len(nums)
-        if length < 3:
-            return []
-        range_a = length - 2
-        range_b = length - 1
-        range_c = length
-        for idx_a in range(0, range_a):
-            a = nums[idx_a]
-            for idx_b in range(idx_a + 1, range_b):
-                b = nums[idx_b]
-                for idx_c in range(idx_a + idx_b + 1, range_c):
-                    c = nums[idx_c]
-                    print('range     c = {}'.format(c))
-                for c in nums[idx_a+idx_b+2:]:
-                    print('enumerate c = {}'.format(c))
-                    if a + b + c == 0:
-                        candidate = sorted([a, b, c])
-                        if candidate not in solution:
-                            solution.append(candidate)
-        return solution
+        nums.sort()
+        result = set()
+        visited = set()
+        length_nums = len(nums)
+        for current in range(length_nums-1):
+            current_value = nums[current]
+            tmp_lookup = {}
+            target = -current_value
+            if current_value not in visited:
+                for runner in range(current+1, length_nums):
+                    runner_value = nums[runner]
+                    if runner_value not in tmp_lookup:
+                        tmp_lookup[target - runner_value] = runner
+                    else:
+                        result.add(
+                            (current_value, target-runner_value, runner_value)
+                        )
+                    visited.add(current_value)
+        return list(result)
