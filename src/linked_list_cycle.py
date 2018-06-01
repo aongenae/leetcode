1ed2de11dc68b1a9e9b0d7644fda42a85820c203
aongenae <arnaud.ongenae@nokia.com>
2018-06-01 10:22:10 -0700

    Problem 141

---
+++ src/linked_list_cycle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+################################################################################
+#
+#   Filename:           linked_list_cycle.py
+#
+#   Author:             Arnaud Ongenae
+#
+#   Leetcode.com:       problem #141
+#
+#   Problem description:
+#   https://leetcode.com/problems/linked-list-cycle/description/
+#
+################################################################################
+
+
+class ListNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+
+    def hasCycle(self, head):
+        '''
+        :type head: ListNode
+        :rtype: bool
+        '''
+        if head is None or head.next is None:
+            return False
+
+        if head.next is head:
+            return True
+
+        current = head
+        runner = current.next
+
+        count = 0
+        while runner.next is not None and runner.next is not current:
+            runner = runner.next
+
+            # current moves also, but twice a slow
+            if count % 2:
+                current = current.next
+            count += 1
+
+        return runner.next is current
---
+++ src/test/test_linked_list_cycle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+################################################################################
+#
+#   Filename:           test_linked_list_cycle.py
+#
+#   Author:             Arnaud Ongenae
+#
+################################################################################
+from ..linked_list_cycle import ListNode, Solution
+from .leetcode_test import LeetcodeTest
+
+
+class TestLinkedListCycle(LeetcodeTest):
+
+    def test_no_node(self):
+        self.assertFalse(Solution().hasCycle(None))
+
+    def test_one_node_no_circle(self):
+        self.assertFalse(Solution().hasCycle(ListNode('n1')))
+
+    def test_one_node_circle(self):
+        n1 = ListNode('n1')
+        n1.next = n1
+        self.assertTrue(Solution().hasCycle(n1))
+
+    def test_three_nodes_one_simple_circle(self):
+        n1 = ListNode('n1')
+        n2 = ListNode('n2')
+        n3 = ListNode('n3')
+        n1.next = n2
+        n2.next = n3
+        n3.next = n1
+        self.assertTrue(Solution().hasCycle(n1))
+
+    def test_three_nodes_no_circle(self):
+        n1 = ListNode('n1')
+        n2 = ListNode('n2')
+        n3 = ListNode('n3')
+        n1.next = n2
+        n2.next = n3
+        self.assertFalse(Solution().hasCycle(n1))
+
+    def test_five_nodes_no_circle(self):
+        n1 = ListNode('n1')
+        n2 = ListNode('n2')
+        n3 = ListNode('n3')
+        n4 = ListNode('n4')
+        n5 = ListNode('n5')
+        n1.next = n2
+        n2.next = n3
+        n3.next = n4
+        n4.next = n5
+        self.assertFalse(Solution().hasCycle(n1))
+
+    def test_five_nodes_simple_circle(self):
+        n1 = ListNode('n1')
+        n2 = ListNode('n2')
+        n3 = ListNode('n3')
+        n4 = ListNode('n4')
+        n5 = ListNode('n5')
+        n1.next = n2
+        n2.next = n3
+        n3.next = n4
+        n4.next = n5
+        n5.next = n1
+        self.assertTrue(Solution().hasCycle(n1))
+
+    def test_five_nodes_mid_course_circle(self):
+        n1 = ListNode('n1')
+        n2 = ListNode('n2')
+        n3 = ListNode('n3')
+        n4 = ListNode('n4')
+        n5 = ListNode('n5')
+        n1.next = n2
+        n2.next = n3
+        n3.next = n4
+        n4.next = n5
+        n5.next = n3
+        self.assertTrue(Solution().hasCycle(n1))
