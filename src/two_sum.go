de8b02811fd39459a76af22522a8654b28df1d8b
aongenae <arnaud.ongenae@nokia.com>
2018-08-02 16:33:16 -0700

    Problem 1 in Go

---
+++ src/two_sum.go
@@ -0,0 +1,25 @@
+################################################################################
+#
+#   Filename:           two_sum.go
+#
+#   Author:             Arnaud Ongenae
+#
+#   Leetcode.com:       problem #1
+#
+#   Problem description:
+#   https://leetcode.com/problems/two-sum/description/
+#
+################################################################################
+
+
+func twoSum(nums []int, target int) []int {
+    buffer := make(map[int]int)
+    for index, value := range nums {
+        if stored_index, ok := buffer[value]; ok {
+            return []int{stored_index, index}
+        } else {
+            buffer[target-value] = index
+        }
+    }
+    return []int{}  // should not reach this line if solution exists
+}
